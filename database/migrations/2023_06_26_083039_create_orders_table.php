c67bc83bf2ba9197569b628249008a76c63a6176
Shalina Riaz <108185986+shalinaariaaz@users.noreply.github.com>
2023-06-26 15:05:26 +0500

    Models created

--- database/migrations/2023_06_26_083039_create_orders_table.php
+++ database/migrations/2023_06_26_083039_create_orders_table.php
@@ -14,7 +14,15 @@ class CreateOrdersTable extends Migration
     public function up()
     {
         Schema::create('orders', function (Blueprint $table) {
-            $table->id();
+            $table->id(order_no);
+            $table->timestamp('order_date')->default(\DB::raw('CURRENT_TIMESTAMP'));
+            $table->bigInteger('username');
+            $table->string('type', 20)->checkIn(['19 ltr', '6 ltr', '1.5 ltr', '0.5 ltr']);
+            $table->integer('quantity');
+            $table->decimal('bottle_price', 10, 2);
+            $table->decimal('total_amount', 10, 2);
+            $table->integer('bill_no')->nullable();
+            $table->foreign('username')->references('username')->on('customer');
             $table->timestamps();
         });
     }
